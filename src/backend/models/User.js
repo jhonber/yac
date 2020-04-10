830cdc936cb01f4e560f3c13436a14bed1b5e0b1
Jhon <jhonb3r@gmail.com>
2020-04-10 17:12:12 -0500

    Add validation for User model

--- src/backend/models/User.js
+++ src/backend/models/User.js
@@ -1,8 +1,20 @@
 const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
 const UserSchema = mongoose.Schema({
-  username: String,
-  password: String,
-  email: String
+  username: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true
+  }
 })
 
+UserSchema.plugin(uniqueValidator)
 module.exports = mongoose.model('User', UserSchema)
