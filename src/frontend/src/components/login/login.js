34005f3e9639a19ddd8ff9aec3d78f07b52574da
Jhon <jhonb3r@gmail.com>
2020-04-12 10:26:40 -0500

    Add login component

---
+++ src/frontend/src/components/login/login.js
@@ -0,0 +1,82 @@
+import React,
+{ useState } from 'react'
+import {
+  Input,
+  Button,
+  Form,
+  FormGroup,
+  Label,
+  Jumbotron,
+  Container,
+  Row,
+  Col
+} from 'reactstrap'
+
+import { postSecure } from '../apiUtils/apiUtils'
+import config from '../../config.json'
+const env = config.env
+const CONFIG = config[env]
+
+const Signup = () => {
+  const [email, setEmail] = useState('jhon2@gmail.com')
+  const [password, setPassword] = useState('1234')
+
+  const handleEmail = (event) => {
+    setEmail(event.target.value)
+  }
+
+  const handlePassword = (event) => {
+    setPassword(event.target.value)
+  }
+
+  const handleSubmit = () => {
+    const url = `${CONFIG.urlBase}${CONFIG.login}`
+    console.log({ url })
+    const data = {
+      email: email,
+      password: password
+    }
+    console.log({ data })
+
+    postSecure(url, data)
+      .then(res => {
+        console.log({ res })
+        if (res) {
+          if (res.ok) {
+            // TODO:
+            // - redirect to home
+          } else {
+            window.alert(res.msg)
+          }
+        } else {
+          window.alert('Something went wrong!')
+        }
+      })
+      .catch(err => console.log({ err }))
+  }
+
+  return (
+    <Container>
+      <Row>
+        <Col md={{ size: 4, offset: 4 }}>
+          <Jumbotron>
+            <h2>Login</h2>
+            <Form>
+              <FormGroup>
+                <Label>Email</Label>
+                <Input type='text' name='email' value={email} onChange={handleEmail} />
+              </FormGroup>
+              <FormGroup>
+                <Label>Password</Label>
+                <Input type='password' name='password' value={password} onChange={handlePassword} />
+              </FormGroup>
+              <Button onClick={handleSubmit}>Login</Button>
+            </Form>
+          </Jumbotron>
+        </Col>
+      </Row>
+    </Container>
+  )
+}
+
+export default Signup
