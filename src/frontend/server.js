adbf077e49c1e03411071e77c801211a54176774
Jhon <jhonb3r@gmail.com>
2020-04-14 11:41:48 -0500

    Change deploy structure to run in Heroku

--- src/frontend/server.js
+++ src/frontend/server.js
@@ -4,16 +4,16 @@ const path = require('path')
 const port = process.env.PORT || 3002
 const app = express()
 
-app.use(favicon(path.join(__dirname, '../build/favicon.ico')))
+app.use(favicon(path.join(__dirname, 'build/favicon.ico')))
 app.use(express.static(__dirname))
-app.use(express.static(path.join(__dirname, '../build')))
+app.use(express.static(path.join(__dirname, 'build')))
 
 app.get('/ping', function (req, res) {
   return res.send('pong')
 })
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, '../build', 'index.html'))
+  res.sendFile(path.join(__dirname, 'build', 'index.html'))
 })
 
 app.listen(port)
