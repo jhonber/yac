e75983bcc3e4bd3d024c29fd542f860809e87230
Jhon <jhonb3r@gmail.com>
2020-04-14 09:09:13 -0500

    Add disabled attribute to avoid sent repeated messages, after multiple times enter key press

--- src/frontend/src/components/textInput/textInput.js
+++ src/frontend/src/components/textInput/textInput.js
@@ -16,25 +16,32 @@ const TextInput = (props) => {
   const youtubeCmd = props.config.youtubeCmd
   const [text, setText] = useState('')
   const [placeholder, setPlaceholder] = useState(props.placeholder)
+  const [disabled, setDisabled] = useState(false)
 
   const handleText = (event) => {
     setText(event.target.value)
   }
 
   const handleSubmit = (event) => {
-    if (event.key === 'Enter') {
+    if (!disabled && event.key === 'Enter') {
+      setDisabled(true)
       if (text.indexOf(youtubeCmd) === 0) {
         const q = text.split(youtubeCmd)[1]
         const urlYoutube = `${props.config.youtube}` +
           `${props.config.youtubeParams}&q=${q}` +
           `&key=${props.config.youtubeApiKey}`
 
-        get(urlYoutube).then(res => {
-          if (res.items && res.items.length > 0) {
-            const videoId = res.items[0].id.videoId
-            makeRequest(youtubeCmd + videoId)
-          }
-        })
+        get(urlYoutube)
+          .then(res => {
+            if (res.items && res.items.length > 0) {
+              const videoId = res.items[0].id.videoId
+              makeRequest(youtubeCmd + videoId)
+            }
+          })
+          .catch(err => {
+            setDisabled(false)
+            console.log('Error: ', err)
+          })
       } else {
         makeRequest(text)
       }
@@ -46,10 +53,15 @@ const TextInput = (props) => {
         .then(res => {
           setText('')
           setPlaceholder('')
+          setDisabled(false)
           console.log({ res })
           if (!res.ok) {
             console.log('Error: ', res.msg)
           }
+        })
+        .catch(err => {
+          setDisabled(false)
+          console.log('Error: ', err)
         })
     }
   }
