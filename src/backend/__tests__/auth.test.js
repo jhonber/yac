1baa8806f95b9ebe6f5124c8fdb9e10caba30665
Jhon <jhonb3r@gmail.com>
2020-04-15 14:42:11 -0500

    Add test for end point /api/login

---
+++ src/backend/__tests__/auth.test.js
@@ -0,0 +1,32 @@
+const supertest = require('supertest')
+const app = require('../app')
+// app.listen(3333)
+const request = supertest(app)
+
+describe('POST /users', () => {
+  it ('login failed', () => {
+    const data = {
+      email: 'test@test.com',
+      password: '1234'
+    }
+
+    const expData = {
+      username: 'test',
+      email: 'test@test.com'
+    }
+
+    return request
+      .post('/api/login')
+      .send(data)
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .then((res) => {
+        const resData = res.body
+
+        expect(resData.ok).toBe(true)
+        expect(typeof resData.token).toBe('string')
+        expect(resData.user).toStrictEqual(expData)
+      })
+  })
+})
